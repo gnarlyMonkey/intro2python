3ca95f8a66672e89d42f39ec1a1c0dc9d67a8b96
anamelie <butterworth828@gmail.com>
2019-02-19 15:17:05 -0800

    amelie's work today

---
+++ anamelie/circle.py
@@ -0,0 +1,9 @@
+# CircleSpirall.py
+import turtle
+t = turtle.Pen( )
+t.speed (0)
+t.pencolor ("blue")
+for x in range (100) : 
+    t.forward (x)
+    t.left(91)
+    
--- anamelie/madlib.py
+++ anamelie/madlib.py
@@ -1,6 +1,7 @@
 #  MadLib.py
 adjective = input ("Please enter an adjective: ")
-verb = input ("Please enter verb ending in -ed: ") 
+verb = input ("Please enter verb ending in -ed: ")
+adjective = input ("Please enter a adjective")
 noun = input("Please enter a noun:  ")
 print("Your MadLib:")
-print ("The" , adjective,  noun, verb,  "over the lazy brown dog.")
+print ("The" , adjective,  noun, verb,  "over the stone bridge.")
